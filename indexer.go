092287e64a38525413e992bc5921ff541c81f379
miguelgz <mmiguel360360@outlook.com>
2023-03-17 21:43:25 -0500

    fixed main class

--- indexer.go
+++ indexer.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/miguelgz36/IndexerGolang/ioindexer"
 	"github.com/miguelgz36/IndexerGolang/record"
 )
 
 func main() {
+	argsWithoutProg := os.Args[1:][0]
+	fmt.Println(argsWithoutProg)
 	data := ioindexer.GetDataBody()
 	record.PostData(data)
 }
